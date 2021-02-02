dcfffcd336a577dd4239aad675e3b96b1b438768
Draganica Ciprian <draganicaciprian@gmail.com>
2021-02-02 18:46:16 +0200

    Implement Service for employee

--- src/main/java/com/codecool/springfinancial/employee/EmployeeController.java
+++ src/main/java/com/codecool/springfinancial/employee/EmployeeController.java
@@ -1,4 +1,8 @@
 package com.codecool.springfinancial.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
 }
--- src/main/java/com/codecool/springfinancial/employee/EmployeeService.java
+++ src/main/java/com/codecool/springfinancial/employee/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.codecool.springfinancial.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 }
