18812e8607d3021d720542090833468e44005433
Draganica Ciprian <draganicaciprian@gmail.com>
2021-02-02 18:57:15 +0200

    Implement Controller for employee, and create a getEmployeeByPid method with the /employee/pid={pid} endpoint

--- src/main/java/com/codecool/springfinancial/employee/EmployeeRepository.java
+++ src/main/java/com/codecool/springfinancial/employee/EmployeeRepository.java
@@ -1,8 +1,14 @@
 package com.codecool.springfinancial.employee;
 
+import com.codecool.springfinancial.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query("SELECT s FROM Employee s WHERE s.pid = ?1")
+    Optional<Employee> findEmployeeByPid(String pid);
 }
--- src/main/java/com/codecool/springfinancial/employee/EmployeeService.java
+++ src/main/java/com/codecool/springfinancial/employee/EmployeeService.java
@@ -2,13 +2,19 @@ package com.codecool.springfinancial.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
     private final EmployeeRepository employeeRepository;
 
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+
+    public Optional<Employee> getEmployeeByPid(String pid) {
+        return employeeRepository.findEmployeeByPid(pid);
     }
 }
