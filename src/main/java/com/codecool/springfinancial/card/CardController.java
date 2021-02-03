1952e26e9cfcd961174528164eb90af5a69a5074
Catalin-code <catalin.despa.code@gmail.com>
2021-02-03 10:02:11 +0200

    Create controller for card.

---
+++ src/main/java/com/codecool/springfinancial/card/CardController.java
@@ -0,0 +1,13 @@
+package com.codecool.springfinancial.card;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardController {
+
+    private final CardService cardService;
+
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+}
